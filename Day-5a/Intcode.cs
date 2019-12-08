0d11eb98f8cde4e5742a481025660a36379583a0
Michael Sanders <mike.gamedev@gmail.com>
2019-12-08 15:29:52 -0800

    Initial commit.

---
+++ Day-5a/Intcode.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Day_5a
+{
+    class Intcode
+    {
+        private int _pointer;
+        private readonly int[] _code;
+
+        private const string AddCommand = "01";
+        private const string MultiplyCommand = "02";
+        private const string InputCommand = "03";
+        private const string OutputCommand = "04";
+        private const string ExitCommand = "99";
+
+        public Intcode(int[] code) => _code = code;
+
+        public void Process()
+        {
+            while (true)
+            {
+                var command = ConsumeOpCode().ToString();
+                var opCode = command.Length < 2 ? "0" + command : command[^2..];
+                var parameters = command.Length > 2 ? command[..^2].PadLeft(4, '0') : "0000";
+
+                if (opCode == AddCommand) Add(parameters);
+                else if (opCode == MultiplyCommand) Multiply(parameters);
+                else if (opCode == InputCommand) Input(parameters);
+                else if (opCode == OutputCommand) Output(parameters);
+                else if (opCode == ExitCommand) return;
+            }
+        }
+
+        private void Add(string parameters)
+        {
+            int a = ConsumeOpCode();
+            int b = ConsumeOpCode();
+            int t = ConsumeOpCode();
+
+            a = parameters[^1] == '0' ? _code.ExpandingGet(a) : a;
+            b = parameters[^2] == '0' ? _code.ExpandingGet(b) : b;
+
+            _code.ExpandingSet(t, a + b);
+        }
+
+        private void Multiply(string parameters)
+        {
+            int a = ConsumeOpCode();
+            int b = ConsumeOpCode();
+            int t = ConsumeOpCode();
+
+            a = parameters[^1] == '0' ? _code.ExpandingGet(a) : a;
+            b = parameters[^2] == '0' ? _code.ExpandingGet(b) : b;
+
+            _code.ExpandingSet(t, a * b);
+        }
+
+        private void Input(string parameters)
+        {
+            int t = ConsumeOpCode();
+
+            Console.WriteLine($"Waiting for input at ({t}): ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            _code.ExpandingSet(t, input);
+        }
+
+        private void Output(string parameters)
+        {
+            int t = ConsumeOpCode();
+
+            int value = parameters[^1] == '0' ? _code.ExpandingGet(t) : t;
+
+            Console.WriteLine($"Value at ({t}): {value}");
+        }
+
+        public int GetValue(int index) => _code[index];
+
+        private int ConsumeOpCode() => _pointer < _code.Length ? _code[_pointer++] : 99;
+    }
+
+    public static class ArrayExtensions
+    {
+        public static void ExpandingSet(this int[] source, int index, int value)
+        {
+            if (index > source.Length) Array.Resize(ref source, index + 1);
+            source[index] = value;
+        }
+        public static int ExpandingGet(this int[] source, int index)
+        {
+            if (index > source.Length) Array.Resize(ref source, index + 1);
+            return source[index];
+        }
+    }
+}
---
+++ Day-5a/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day_5a
+{
+    class Program
+    {
+        private const string InputPath = @"day5a-input.txt";
+
+        static void Main(string[] args)
+        {
+            int[] code = File.ReadAllText(InputPath).Split(",").Select(int.Parse).ToArray();
+
+            //code = new[] { 1002, 4, 3, 4, 33 };
+
+            Intcode processor = new Intcode(code);
+            processor.Process();
+        }
+    }
+}
